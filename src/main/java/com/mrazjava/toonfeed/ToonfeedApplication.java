2a0491d596d0ed1df24631acfd03650aff31482c
Adam Zimowski <mrazjava@protonmail.com>
2021-02-26 14:50:55 +0100

    initial checkin based on start.spring.io

---
+++ src/main/java/com/mrazjava/toonfeed/ToonfeedApplication.java
@@ -0,0 +1,13 @@
+package com.mrazjava.toonfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToonfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToonfeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrazjava/toonfeed/ToonfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrazjava.toonfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToonfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
