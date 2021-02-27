ccd76eba423ca50d846d9dba314565b991dbb1a3
Adam Zimowski <mrazjava@protonmail.com>
2021-02-27 15:57:28 +0100

    use single yml source for log cfg

--- src/main/java/com/mrazjava/toonfeed/pdl/PdlToonProvider.java
+++ src/main/java/com/mrazjava/toonfeed/pdl/PdlToonProvider.java
@@ -12,10 +12,13 @@ import org.springframework.stereotype.Component;
 import com.mrazjava.toonfeed.ToonModel;
 import com.mrazjava.toonfeed.ToonProvider;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author mrazjava
  * @see <a href="http://feeds.feedburner.com/PoorlyDrawnLines">(P)oorly (D)rawn (L)ines</a>
  */
+@Slf4j
 @Component("PdlToonProvider")
 public class PdlToonProvider implements ToonProvider, MessageHandler {
     
@@ -33,6 +36,7 @@ public class PdlToonProvider implements ToonProvider, MessageHandler {
     @Override
     public void handleMessage(Message<?> message) throws MessagingException {
         ToonModel toon = (ToonModel)message.getPayload();
+        log.info("handled: {}", toon);
         toons.add(toon);
     }
 
