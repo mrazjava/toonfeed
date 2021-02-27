4127c750a507d169f0b93859a8b0081c9964c591
Adam Zimowski <mrazjava@protonmail.com>
2021-02-27 17:06:56 +0100

    cfg pull delay for XKCD

--- src/main/java/com/mrazjava/toonfeed/xkcd/XkcdConfig.java
+++ src/main/java/com/mrazjava/toonfeed/xkcd/XkcdConfig.java
@@ -1,6 +1,7 @@
 package com.mrazjava.toonfeed.xkcd;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.config.EnableIntegration;
@@ -21,10 +22,11 @@ public class XkcdConfig {
     public IntegrationFlow xkcdFlow(
             XkcdMessageSourceSpec msgSourceSpec,
             XkcdTransformer transformer,
-            @Qualifier("XkcdProvider") MessageHandler handler) {
+            @Qualifier("XkcdProvider") MessageHandler handler,
+            @Value("${toon.xkcd.pullDelayMs:500}") long delay) {
 
         return IntegrationFlows
-                .from(msgSourceSpec, e -> e.poller(p -> p.fixedDelay(100)))
+                .from(msgSourceSpec, e -> e.poller(p -> p.fixedDelay(delay)))
                 .transform(transformer)
                 .handle(handler)
                 .get();
