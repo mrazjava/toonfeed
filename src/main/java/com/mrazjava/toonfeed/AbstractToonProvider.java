36857f8f9220de00429c27421b4499dd76c4a52d
Adam Zimowski <mrazjava@protonmail.com>
2021-02-28 12:52:24 +0100

    implement provider fetch limits

--- src/main/java/com/mrazjava/toonfeed/AbstractToonProvider.java
+++ src/main/java/com/mrazjava/toonfeed/AbstractToonProvider.java
@@ -10,10 +10,21 @@ import org.springframework.messaging.MessagingException;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Provides collected cartoon data from some source for further consumption. The 
+ * data is collected up to {@link #getFetchLimit()}. Once limit is reached but  
+ * more data is consumed, the oldest data will be removed to make room for the 
+ * newly consumed data while respecting the {@code #getFetchLimit()}.
+ * 
+ * Data is collected by consumption of Spring {@link Message} that can be fed by 
+ * any means supported by the framework (eg: Spring DSL integration).
+ * 
+ * @author mrazjava
+ */
 @Slf4j
 public abstract class AbstractToonProvider implements ToonProvider, LimitedFetching, MessageHandler {
 
-    protected List<ToonModel> toons = new LinkedList<>();
+    protected LinkedList<ToonModel> toons = new LinkedList<>();
     
     @Override
     public List<ToonModel> getToons() {
@@ -22,8 +33,18 @@ public abstract class AbstractToonProvider implements ToonProvider, LimitedFetch
 
     @Override
     public void handleMessage(Message<?> message) throws MessagingException {
+        
         ToonModel toon = (ToonModel)message.getPayload();
-        log.info("handled: {}", toon);
+        
+        if(log.isInfoEnabled()) {
+            log.info("[{}] handling: {}", getClass().getSimpleName(), toon);
+        }
+        
+        if(toons.size() == getFetchLimit()) {
+            ToonModel ejectedToon = toons.removeFirst();
+            log.info("fetch-limit[{}] reached; ejected oldest toon: {}", getFetchLimit(), ejectedToon);
+        }
+        
         toons.add(toon);
     }
 }
