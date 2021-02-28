88be0ef4b8f134ad42560449cf071d9c73ee3e68
Adam Zimowski <mrazjava@protonmail.com>
2021-02-28 11:10:11 +0100

    minor tweaks to trigger base

--- src/main/java/com/mrazjava/toonfeed/AbstractToonfeedTrigger.java
+++ src/main/java/com/mrazjava/toonfeed/AbstractToonfeedTrigger.java
@@ -2,6 +2,7 @@ package com.mrazjava.toonfeed;
 
 import java.util.Date;
 
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.Trigger;
@@ -9,12 +10,24 @@ import org.springframework.scheduling.TriggerContext;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Base for a custom spring integration polling trigger. Useful for 
+ * toon feed implementations pulling with Spring DSL.
+ * 
+ * This base understands initial fetch size from a data feed. Deriving classes 
+ * only need to provide delays for intial fetch and subsequent fetch of 
+ * new data.
+ * 
+ * @author mrazjava
+ */
 @Slf4j
 public abstract class AbstractToonfeedTrigger implements Trigger {
 
+    public static final String DATE_PATTERN = "yyyy-MM-dd.HH.mm.ss.SS";
+    
     protected int count = 0;
     
-    @Value("${toon.init-fetch-size}")
+    @Value("${toon.init-fetch-size:10}")
     private int initFetchSize;
     
     @Override
@@ -23,16 +36,25 @@ public abstract class AbstractToonfeedTrigger implements Trigger {
         count++;
         
         Date lastExecuted = triggerContext.lastCompletionTime();
-        
-        log.debug("[{}] last executed: {}", getTriggerName(), lastExecuted);
+        Date nextExecution;
         
         if(lastExecuted == null) {
-            return DateUtils.addMilliseconds(new Date(), getInitialDelayMs());
+            nextExecution = DateUtils.addMilliseconds(new Date(), getInitialDelayMs());
+            log.debug("[{}] first execution scheduled for: {}",
+                    getTriggerName(), DateFormatUtils.format(nextExecution, DATE_PATTERN));
+            return nextExecution;
         }
         
-        return count > initFetchSize ? 
+        nextExecution = count > initFetchSize ? 
                 DateUtils.addMinutes(lastExecuted, getPullDelayMin()) : 
-                DateUtils.addMilliseconds(lastExecuted, getInitialDelayMs());
+                DateUtils.addMilliseconds(lastExecuted, getInitialDelayMs());            
+
+        log.debug("[{}] last executed: {} | next execution: {}", 
+                getTriggerName(), 
+                DateFormatUtils.format(lastExecuted, DATE_PATTERN), 
+                DateFormatUtils.format(nextExecution, DATE_PATTERN));
+        
+        return nextExecution;
     }
     
     protected abstract String getTriggerName();
