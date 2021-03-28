9ebdf3cc926aa620f1e560bd6cbac430eba306cb
Adam Zimowski <mrazjava@protonmail.com>
2021-03-28 20:28:10 +0200

    integrated AbstractToonProvider base directly into impl
    Since provider is now controlled via dynamic configuration, abstract
    base is no longer necessary.

--- src/main/java/com/github/mrazjava/toonfeed/ToonProviderImpl.java
+++ src/main/java/com/github/mrazjava/toonfeed/ToonProviderImpl.java
@@ -1,10 +1,33 @@
 package com.github.mrazjava.toonfeed;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
-public class ToonProviderImpl extends AbstractToonProvider {
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Provides collected cartoon data from some source for further consumption. The 
+ * data is collected up to {@link #getFetchLimit()}. Once limit is reached but  
+ * more data is consumed, the oldest data will be removed to make room for the 
+ * newly consumed data while respecting the {@code getFetchLimit()}.
+ * 
+ * Data is collected by consumption of Spring {@link Message} that can be fed by 
+ * any means supported by the framework (eg: Spring DSL integration).
+ * 
+ * @author mrazjava
+ */
+@Slf4j
+public class ToonProviderImpl implements ToonProvider, LimitedFetching, MessageHandler {
 
     public static final int DEFAULT_FETCH_LIMIT = 5;
+    
+    protected LinkedList<ToonModel> toons = new LinkedList<>();
     
     private int fetchLimit;
     
@@ -34,7 +57,28 @@ public class ToonProviderImpl extends AbstractToonProvider {
     }
 
     @Override
-    protected String getProviderName() {
-        return StringUtils.isEmpty(name) ? super.getProviderName() : name;
+    public List<ToonModel> getToons() {
+        return Collections.unmodifiableList(toons);
+    }
+
+    @Override
+    public synchronized void handleMessage(Message<?> message) throws MessagingException {
+        
+        ToonModel toon = (ToonModel)message.getPayload();
+        
+        if(log.isInfoEnabled()) {
+            log.info("[{}] handling: {}", getProviderName(), toon);
+        }
+        
+        if(toons.size() == getFetchLimit()) {
+            ToonModel ejectedToon = toons.removeFirst();
+            log.info("fetch-limit[{}] reached; ejected oldest toon: {}", getFetchLimit(), ejectedToon);
+        }
+        
+        toons.add(toon);
+    }
+    
+    public String getProviderName() {
+        return StringUtils.isEmpty(name) ? getClass().getSimpleName() : name;
     }
 }
